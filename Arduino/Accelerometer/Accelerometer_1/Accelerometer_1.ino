c56110dd4157797130ae15c073d4c08c354e9294
Taslim <44072484+Taslim-M@users.noreply.github.com>
2019-11-04 22:18:28 +0400

    Bug fixes

--- Arduino/Accelerometer/Accelerometer_1/Accelerometer_1.ino
+++ Arduino/Accelerometer/Accelerometer_1/Accelerometer_1.ino
@@ -14,21 +14,18 @@ double prevAvg = 0;
 unsigned long  int prevT, presentT;
 // for low-pass filter - init to zero
 double dataY[3];
-int score;
-byte message;
-// keeps the score of the player
-struct LED_State{
-//int speed;
-bool criticalRegion;
 
-};
+int score; // to maintain score 
+
 LED_State LED;
+
+//Context for states to decide
 struct Context {
   char currentState;
   byte message;
 };
-
 Context Acc_Context;
+
 //***********************Setup***********************
 void setup() {
   // initialize the serial communications:
@@ -39,10 +36,58 @@ void setup() {
   presentT = prevT = millis();
   Acc_Context.currentState = 'w'; // initially the current state is wait to start
   score = 0;
-  
 
 }
 
+//***********************Loop***********************
+void loop() {
+
+  if (mySerial.available()) { //check for the beginning and end of game
+    Acc_Context.message = (byte)mySerial.read(); // read the Data sent as byte
+  }
+  //State Design Pattern
+  switch (Acc_Context.currentState) {
+    case 'w':
+      if (Acc_Context.message == 0b11111111) //if Java sent 1111 1111 - game started so change state to game on
+      {
+        Acc_Context.currentState = 'g';
+      }
+      break;
+
+    //Send final point one game over.
+    case 'g':
+      if (Acc_Context.message == 0b00000000) { //if Java sent 0- send final points
+        mySerial.write((byte) score); // send final score to Java
+        score =0;
+        Acc_Context.currentState = 'w'; // change to wait state
+      }
+      else {
+        dataY[2] = convertToG(analogRead(ypin));
+        // [2]store the time when the value was read
+        presentT = millis();
+
+        //[3] calculate moving average (low-pass filter) for smoothening the data
+        double avgYG = movingAverage(dataY[0], dataY[1], dataY[2]);
+
+        //[4]Calculate the Jerk
+        double jerk = convertToJerk(avgYG, prevAvg);
+
+        // [5]if a jump is detected (i.e jerk>=5) and no signal for jump has been transmitted in the last one second then send a signal
+        if (jerk >= 5 && (presentT > (prevT + 1000))) {
+
+          evaluateJump();
+
+          // update score and signal to java so that java can intensify the sound
+          //mySerial.write(B00001111); //00001111 XY00ZZZZ -> X represent player ID, Y is the Accelerometer id, 1111->successful jump
+
+          // store the present time for sending a byte for the next valid jump
+          prevT = presentT;
+        } // end jump detected
+
+        delay(100);// constant delay between the readings
+      }
+  }//end switch
+}
 
 //******************Functions**********************
 
@@ -66,83 +111,24 @@ double convertToJerk(double currentAvg, double &prevAvg) {
   return jerk;
 }
 
- void increaseScore(int&score, byte message){
-message= message&& 0b00001111;
-score+= 2*(int)message;
-  
- }
- void decreaseScore(int&score, byte message){
-message= message&& 0b00001111;
-score-=(int)message;
-  
- }
- 
-
-//keep running forever
-void loop() {
-
-  if (mySerial.available()) { //check for the beginning and end of game
-    Acc_Context.message = (byte)mySerial.read(); // read the Data sent as byte - can read from other end node
+//Evaluate jump
+void evaluateJump() {
+  if ((Acc_Context.message & B01000000) == 0) { // jumped in non critical region - WRONG
+    mySerial.write(B00000001); //00001111 XY00ZZZZ -> X represent player ID, Y is the Accelerometer id, 0001->wrong jump
+    decreaseScore();
+  }
+  else if ((Acc_Context.message & B01000000) != 0) // ensure the message is meant for current player by its device (XY check)
+  {
+    mySerial.write(B00000010); //00001111 XY00ZZZZ -> X represent player ID, Y is the Accelerometer id, 0010-> correct jump
+    increaseScore(Acc_Context.message );
   }
 
-  switch (Acc_Context.currentState) {
-    case 'w':
-      if (Acc_Context.message == 0b11111111) //if Java sent 1111 1111 - game started so send accelerometer data
-      { Acc_Context.currentState = 'g';
+}
+void increaseScore(byte message) { // Increase score depending on speed (multiplier)
+  unsigned int multiplier = (message & 0b00001111);
+  score += 2 * multiplier;
+}
+void decreaseScore() {
+  score -= 1; // decrease 1 point for missing jump
 
-      }
-      break;
-
-    //Send data to Java once Game started, otherwise keep checking
-    case 'g':
-      if (Acc_Context.message == 0b00000000) { //if Java sent 0 - game ended so don't send any data
-        // send score
-          
-        mySerial.write((byte) score);
-
-        Acc_Context.currentState = 'w';
-      }
-      else {
-        dataY[2] = convertToG(analogRead(ypin));
-        // [2]store the time when the value was read
-        presentT = millis();
-
-        if (mySerial.available()) { //check for the beginning and end of game
-          message = (byte)mySerial.read(); // read the Data sent as byte - can read from other end node
-          // take the message and update the game
-        }
-
-        //[3] calculate moving average (low-pass filter) for smoothening the data
-        double avgYG = movingAverage(dataY[0], dataY[1], dataY[2]);
-
-        //[4]Calculate the Jerk
-        double jerk = convertToJerk(avgYG, prevAvg);
-
-        // [5]if a jump is detected (i.e jerk>=5) and no signal for jump has been transmitted in the last one second then send a signal
-        if (jerk >= 5 && (presentT > (prevT + 1000))) {
-
-          if(LED.criticalRegion){
-            // send to java
-            mySerial.write(B00000010); //00001111 XY00ZZZZ -> X represent player ID, Y is the Accelerometer id, 0010-> correct jump
-              // increase the score according to the last 4 bytes of the message;
-              increaseScore(score, message);
-          }
-          else {
-            // send to java
-             mySerial.write(B00000001); //00001111 XY00ZZZZ -> X represent player ID, Y is the Accelerometer id, 0001->wrong jump
-              // decrease the score the score
-              decreaseScore(score, message);
-          }
-
-          // update score and signal to java so that java can intensify the sound 
-          //mySerial.write(B00001111); //00001111 XY00ZZZZ -> X represent player ID, Y is the Accelerometer id, 1111->successful jump
-
-          // store the present time for sending a byte for the next valid jump
-          prevT = presentT;
-        }
-        //delay between the readings
-        delay(100);
-      }
-
-  }
 }
