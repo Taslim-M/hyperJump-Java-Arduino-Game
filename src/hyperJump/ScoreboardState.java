9aa47881d78d1bc13dc81d2982fa1c1b10038731
PriyankaPrakashChand <53374732+PriyankaPrakashChand@users.noreply.github.com>
2019-11-10 16:04:05 +0400

    ---

--- src/hyperJump/ScoreboardState.java
+++ src/hyperJump/ScoreboardState.java
@@ -1,5 +1,6 @@
 package hyperJump;
 import java.io.*;
+import java.util.Scanner;
 public class ScoreboardState implements GameState {
 ByteDecoder bD;
 boolean anotherRound; // false by Default
@@ -20,14 +21,9 @@ boolean anotherRound; // false by Default
 			System.out.println("Final Scores: "+(int)context.currentMsg.getPayLoad());
 			// ask the user input if the want to play another round
 			System.out.println("Please press y if you wish to play an other round ");
-			 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-			 int input= -1;
-			try {
-				input = in.read();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			Scanner in = new Scanner(System.in);
+			 char input = sc.next().charAt(0); 
+			
 			 if(input=='y' | input=='Y') {
 				 context.setState(new PlayerRegistrationState());
 				 
