8172e27acd6f59e89bf40753f40f35fc0a2b78fb
PriyankaPrakashChand <53374732+PriyankaPrakashChand@users.noreply.github.com>
2019-11-16 22:45:33 +0400

    only comments added to byteDecoder

--- src/hyperJump/ByteDecoder.java
+++ src/hyperJump/ByteDecoder.java
@@ -8,12 +8,19 @@ public final class ByteDecoder {
 	}
 
 	public static void updateFlags(msg m) {
-		byte b = m.getPayLoad();
-		if (b == (byte) 0b00000000) {
+		byte b = m.getPayLoad();  // store the payload in a temporary byte
+		
+		// if the byte ==0 -> game over so turn on the gameTimeOver flag
+		if (b == (byte) 0b00000000) { 
 			gameTimeOver = true;
-		} else if (b == (byte) 0b00000001) {
+		} 
+		// if the byte ==1 -> Invalid Jump so turn on invalidJumpDetected flag
+		else if (b == (byte) 0b00000001) {
 			invalidJumpDetected = true;
-		} else if (b == (byte) 0b000000010) {
+		} 
+		 
+		// if the byte ==2 -> valid Jump so turn on validJumpDetected flag
+		else if (b == (byte) 0b000000010) {
 			validJumpDetected = true;
 		}
 
